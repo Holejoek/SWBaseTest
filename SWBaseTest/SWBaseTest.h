e1f4a0a1ccedaddf7e2c06f7dee2297df8db13bb
holejoek <eggman12363@gmail.com>
2022-12-06 15:41:17 +0500

    init commit

---
+++ SWBaseTest/SWBaseTest.h
@@ -0,0 +1,18 @@
+//
+//  SWBaseTest.h
+//  SWBaseTest
+//
+//  Created by Иван Тиминский on 06.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SWBaseTest.
+FOUNDATION_EXPORT double SWBaseTestVersionNumber;
+
+//! Project version string for SWBaseTest.
+FOUNDATION_EXPORT const unsigned char SWBaseTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWBaseTest/PublicHeader.h>
+
+
